1324c2c10ab181ee26f45ae5536f316d3c12bbf9
SamirHumagain <152156586+SamirHumagain@users.noreply.github.com>
2024-06-06 08:16:33 +0545

    Add files via upload

---
+++ stringMethod.js
@@ -0,0 +1,18 @@
+var a="this is bsc csit";
+console.log(a);
+
+//print the length of the string
+console.log("the length of the stirng is",a.length);
+
+//print the index of a string 
+console.log("the index of string is",a.charAt(4));
+
+//print the character code of the string at given index
+console.log("the character code of the given string is",a.charCodeAt(5));
+
+//print the sliced text of the string
+console.log("the sliced part of the string is",a.slice(4,7));
+
+//print the substring of the string
+console.log("the sub string of the string is",a.substring(1,5));
+
